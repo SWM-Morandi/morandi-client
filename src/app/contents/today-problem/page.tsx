688a522440249313efc491a410dab967274a40c0
yeobi01 <yeobi_01@naver.com>
2024-03-07 21:13:47 +0900

    feat: 페이지에 랭킹 컴포넌트 추가

--- src/app/contents/today-problem/page.tsx
+++ src/app/contents/today-problem/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/app/contents/today-problem/_component/Banner";
 import TodayProblemList from "@/app/contents/today-problem/_component/TodayProblemList";
+import RankingList from "@/app/contents/today-problem/_component/RankingList";
 
 export default function TodayProblem() {
   return (
     <div className="flex flex-col justify-center items-center w-full max-w-[80rem]">
       <Banner />
       <TodayProblemList />
+      <RankingList />
     </div>
   );
 }

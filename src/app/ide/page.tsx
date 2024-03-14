ac8e03b33704262f3840eea3ba58dc7c9248f136
yeobi01 <yeobi_01@naver.com>
2024-03-14 09:56:51 +0900

    feat: ide 페이지 제작

---
+++ src/app/ide/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Header from "@/app/ide/_component/Header";
+import ProblemInfo from "@/app/ide/_component/ProblemInfo";
+import Editor from "@/app/ide/_component/Editor";
+import SideBar from "@/app/ide/_component/Sidebar";
+import InputOuputBox from "./_component/InputOuputBox";
+
+export default function IDE() {
+  return (
+    <div className="bg-gray-900 text-white h-[100vh]">
+      <div className="flex flex-row">
+        <SideBar />
+        <div className="flex flex-col">
+          <Header />
+          <div className="flex flex-row px-[2rem]">
+            <ProblemInfo />
+
+            <div className="flex flex-col w-[53vw]">
+              <Editor />
+              <InputOuputBox />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
